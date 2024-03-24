4dd840848031c489edb21a377917f2366eb2f54a
jayson <jaysonshammah3@gmail.com>
2024-03-24 18:31:30 +0300

    add net salary

--- netsalary.js
+++ netsalary.js
@@ -1,26 +1,65 @@
-function tax(incomeSalary){
-    let taxed = 0;
+function show(value){
+    console.log(value);
+}
+
+// input = Basic salary 30,000
+// input = Benefits 4,200
+
+let basicSalary = 600000;
+let benefit = 4200;
+
+// 
+
+//output = tax
+function tax(taxableIncome){
+    let paye = 0;
     let grossTax = 0;
     let rated = 0; 
-    if(incomeSalary > 0 && incomeSalary <= 24000){
-        grossTax = (incomeSalary*0.10);
-        taxed = incomeSalary - grossTax;
-    }else if (incomeSalary >= 24001 && incomeSalary <= 32333){
-        rated = (incomeSalary - 24000) * 0.25;
+    let personalRelief = 2400;
+
+    if(taxableIncome > 0 && taxableIncome <= 24000){
+        grossTax = (taxableIncome*0.10);
+    }else if (taxableIncome >= 24001 && taxableIncome <= 32333){
+        rated = (taxableIncome - 24000) * 0.25;
         grossTax = 2400 + rated
-        taxed = incomeSalary-grossTax;
-    }else if (incomeSalary >= 32334 && incomeSalary <= 500000){
-        rated = (incomeSalary - 32333) * 0.30;
+    }else if (taxableIncome >= 32334 && taxableIncome <= 500000){
+        rated = (taxableIncome - 32333) * 0.30;
         grossTax = 2400 + 2083.25 + rated
-        taxed = incomeSalary-grossTax;
-    }else if (incomeSalary >= 500001 && incomeSalary <= 800000){
-        rated = (incomeSalary - 500000) * 0.325;
-        grossTax = 2400 + 2083.25 + 140300.1 + rated
-        taxed = incomeSalary-grossTax;  
-    }else if(incomeSalary > 800000){
-        rated = (incomeSalary - 800000) * 0.35;
+    }else if (taxableIncome >= 500001 && taxableIncome <= 800000){
+        rated = (taxableIncome - 500000) * 0.325;
+        grossTax = 2400 + 2083.25 + 140300.1 + rated  
+    }else if(taxableIncome > 800000){
+        rated = (taxableIncome - 800000) * 0.35;
         grossTax = 2400 + 2083.25 + 140300.1 + 97500 + rated
-        taxed = incomeSalary-grossTax;
     }
-    return taxed;
+    paye =  ~~(grossTax - personalRelief);
+    netsalary = (taxableIncome - paye);
+  return { paye, netsalary } 
 }
+show(tax())
+
+//output = NHIFDeductions
+
+//output = NSSFDeductions
+
+//output = Gross salary
+
+//output = Net Salary
+/*
+
+gross salary = 75000
+
+nssf = (75000 * 0.06) = 4500
+nhif = 1400 //(1400 * 0.15) = 210 * 
+75000 - (nssf + nhif)
+let taxableIncome = 75000 - (nssf + nhif)5900 //69100
+let payee = tax(taxableIncome).paye
+let net salary = tax(taxableIncome).netsalary
+
+NSSFDeductions = 4500
+NHIFDeductions = 1400
+payee (i.e. Tax) = 51186.65 // tax.paye
+gross salary = 75000
+net salary = 17913.35 // tax.netsalary
+
+*/
